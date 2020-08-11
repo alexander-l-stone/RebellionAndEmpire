69a63f48e27458774bb4acf3e75208e16dcfaff6
Alex Stone <aloecend@gmail.com>
2020-08-10 21:59:42 -0700

    Updated hexcode and added a test scene.

--- scenes/hex/hex.gd
+++ scenes/hex/hex.gd
@@ -1,14 +1,17 @@
 extends Node
 
+# Use values from 0 to 1
+export var red = 0.0
+export var green = 0.0
+export var blue = 0.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var x = 0
+export var y = 0
+export var z = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite_Hex.modulate = Color(red, green, blue)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
