04aaaa82ef1a101bbdbe89ca669aefa07a47f7b8
Alex Stone <aloecend@gmail.com>
2020-12-05 17:15:13 -0800

    WIP: renamed files and variables for consistency.

--- scenes/planet_details/scenes/scripts/fleet_details.gd
+++ scenes/planet_details/scenes/scripts/fleet_details.gd
@@ -5,7 +5,7 @@ var fleet = null
 var index: int = 0
 
 var ship_stack_scene = load("res://scenes/planet_details/scenes/ship_stack.tscn")
-var NewFleetButton = load("res://scenes/planet_details/scenes/NewFleetButton.tscn")
+var new_fleet_button = load("res://scenes/planet_details/scenes/new_fleet_button.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,10 +26,10 @@ func set_display(ships):
 		ship_stack.ship_number = num_ship_type
 		ship_stack.fleet = fleet
 		$FleetDetails_GridContainer.add_child(ship_stack)
-	var butt = NewFleetButton.instance()
-	butt.r = fleet.r
-	butt.q = fleet.q
-	butt.index = index
-	$FleetDetails_GridContainer.add_child(butt)
+	var button = new_fleet_button.instance()
+	button.r = fleet.r
+	button.q = fleet.q
+	button.index = index
+	$FleetDetails_GridContainer.add_child(button)
 
 
