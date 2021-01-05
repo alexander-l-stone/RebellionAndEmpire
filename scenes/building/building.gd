dbb25d08d6e2655ae7be551c08104f737ca62b32
Richard Nicholson <twilly0@gmail.com>
2021-01-05 04:26:18 +0000

    =tweaked some colors, made buildings different in space and ground

--- scenes/building/building.gd
+++ scenes/building/building.gd
@@ -4,19 +4,10 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var sprite_path = "res://resources/heavy_industry.png"
+var sprite_path = "res://resources/light_industry.png"
 
 var building_type = "industry"
 
 var cost = 10
 
-var building_effect = "produce-1:I"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.texture = load(sprite_path)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var building_effect = "I:1"
